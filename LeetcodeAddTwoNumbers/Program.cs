01afdc3f85a9cac0ff353ec4d985d48453aee2e9
Candace Williams <candace.williams94@hotmail.com>
2021-09-28 17:36:54 -0400

    adding the other projects for assignment

---
+++ LeetcodeAddTwoNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetcodeAddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Solution
+    {
+        public ListNode AddTwoNumbers (ListNode l1, ListNode l2)
+        {
+            ListNode head = new ListNode();
+            ListNode head = null;
+            ListNode tail = head;
+            while (l1 != null || l2 != null)
+            {
+                if (head == null)
+                {
+                    tail = head = new ListNode(total);
+                }
+                else
+                {
+                    tail = tail.next = new ListNode(total);
+                }
+            }           
+            
+        }
+    }
+}
---
+++ LeetcodeReverseLinkedList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LeetcodeReverseLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Solution
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode curr = head;
+            ListNode next;
+
+            while (curr != null)
+            {
+                // You want to change curr.next to equal the previous item, not the next item. 
+                // 1. Store curr.next
+                var temp = curr.next;
+                // 2. Change curr.next to point to the previous value
+                curr.next = prev;
+                // 3. Ready the loop for the next pass by updating prev
+                prev = curr;
+                // 4. Ready the loop for the next pass by updating curr.
+                curr = temp;
+            }
+
+            return prev;
+        }
+
+
+    }
+}
